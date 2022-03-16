b65d15ae8002d961aba25d55c49b5c690d57c7fa
AndrewChen116 <andrew199786821@gmail.com>
2022-03-16 12:41:28 +0800

    revise the example setting and standardize the name of terms

--- server.R
+++ server.R
@@ -1,23 +1,30 @@
+rm(list = ls())
 source("FUN_GenomeSite_comparison.R")
+load("default.RData")
 options(shiny.maxRequestSize=30*1024^2) 
 
 server <- function(input, output, session) {
-  
-  # Start time
-  # start_time <- eventReactive(c(input$DoSearch, input$DoExample),{
-  #   cat("start_time\n")
-  #   t1 <- proc.time()
-  # })
+ 
+  # check if apply example
+  observeEvent(input$doExample,{
+    assign("doExample",TRUE,globalenv())
+    cat("Example setting checked\n")
+  })
   
   # Anchor and Target table input 
-  Anchor_Table <- eventReactive(c(input$DoSearch, input$anchor_file, input$DoExample),{
-    cat("Anchor_Table\n")
+  anchor_table <- eventReactive(c(input$doSearch, input$anchor_file, input$doExample),{
+    ## record the start time
     t1 <- proc.time()
+    cat("Import example:", doExample,"\n")
     tryCatch(
       {
-        if(!is.null(input$anchor_file$datapath)){
-            anchor.df <- input$anchor_file$datapath %>% read.csv(sep = "\t") 
+        if(!is.null(input$anchor_file$datapath)& !doExample){
+          anchor.df <- input$anchor_file$datapath %>% read.csv(sep = "\t") 
+          cat("Imported anchor file checked\n")
+        }else{
+          cat("Example anchor file checked\n")
         }
+        colnames(anchor.df) <- c("id","chr","start","end","strand")
       },
       warning = function(war){
         print(war)
@@ -32,13 +39,16 @@ server <- function(input, output, session) {
       "t1"=t1
     )
   })
-  Target_Table <- eventReactive(c(input$DoSearch, input$target_file, input$DoExample),{
-    cat("Target_Table\n")
+  target_table <- eventReactive(c(input$doSearch, input$target_file, input$doExample),{
     tryCatch(
       {
-        if(!is.null(input$target_file$datapath)){
+        if(!is.null(input$target_file$datapath) & !doExample){
           target.df <- input$target_file$datapath %>% read.csv(sep = "\t") 
+          cat("Imported target file checked\n")
+        }else{
+          cat("Example target file checked\n")
         }
+        colnames(target.df) <- c("id","chr","start","end","strand")
       },
       warning = function(war){
         print(war)
@@ -52,29 +62,30 @@ server <- function(input, output, session) {
   })
   
   # Comparison  
-  output_Table <- eventReactive(c(input$DoSearch, input$DoExample),{
-    cat("output_Table\n")
-    #start_time()
-    anchor.lt <- Anchor_Table()
+  output_Table <- eventReactive(c(input$doSearch, input$doExample),{
+    anchor.lt <- anchor_table()
     anchor.df <- anchor.lt$anchor
     t1 <- anchor.lt$t1
-    target.df <- Target_Table()
+    target.df <- target_table()
     
     output.df <- NULL
     tryCatch(
       {
-        if(T){
-          
+        if(!is.null(anchor.df) & !is.null(target.df)){
+          cat("Comparing ...\n")
+        
           lt <- list(
             "anchor" = anchor.df,
             "target" = target.df
           )
           
           output.df <- Genomesite_comparison(
-            lt, f_nt = input$f_nt, b_nt = input$b_nt,
-            applyParallel = as.logical(input$DoParallel),
+            lt, up_nt = input$up_nt, down_nt = input$down_nt,
+            applyParallel = as.logical(input$doParallel),
             core = input$ncore
           ) %>% as_tibble()
+          
+          cat("Comparation finished \n")
         }else{
           output.df <- NULL
         }
@@ -89,67 +100,26 @@ server <- function(input, output, session) {
       }
     )
     
+    ## record the execution time
     t2 <- proc.time()
     t <- t2-t1
     print_time <- paste0("Execution time: ",round(t[3][[1]],2), " s")
     
+    ## reset doExample
+    cat("Reset example setting\n")
+    assign("doExample",FALSE,globalenv())
+    
+    ## return
     list(
       "output" = output.df,
       "time" = print_time
     )
-    
+    cat("******************** \n")
   })
-  
-  # End time 
-  # print_time <- eventReactive(c(input$DoSearch, input$DoExample),{
-  #   cat("print_time\n")
-  #   t1 <- start_time()
-  #   t2 <- proc.time()
-  #   t <- t2-t1
-  #   print_time <- paste0("Execution time: ",round(t[3][[1]],2), " s")
-  #   
-  #   print_time
-  # })
-  
-  ## Check
-  # info <- NULL
-  # print_A <- reactive({
-  #   if(is.null(input$xmlfile$datapath)){
-  #     info <- NULL
-  #   }else if(c(!grepl(".xml$",input$xmlfile$datapath)) %>% as.numeric() %>% sum()){
-  #     info <- '<font color="red">Wrong format!</font>'
-  #   }else if(is.null(xmlTable())){
-  #     info <- '<font color="red">Please check again!</font>'
-  #   }else{
-  #     info <- "Successfully input data"
-  #   }
-  #   HTML(info)
-  # })
-  # 
-  # 
-  # print_T <- reactive({
-  #   if(is.null(input$jsonfile$datapath)){
-  #     info <- NULL
-  #   }else if(c(!grepl(".json$",input$jsonfile$datapath)) %>% as.numeric() %>% sum()){
-  #     info <- '<font color="red">Wrong format</font>'
-  #   }else if(is.null(jsonTable())){
-  #     info <- '<font color="red">Please check again!</font>'
-  #   }else{
-  #     info <- "Successfully input data"
-  #   }
-  #   HTML(info)
-  # })
-  # 
- 
-  ## Output setting 
-  # output$print_A <- renderUI(
-  #   print_A()
-  # )
-  # output$print_T <- renderUI(
-  #   print_T()
-  # )
 
+  # export to ui
   
+  ## comparation result
   output$output_table <- renderDataTable(
     output_Table()[[1]],
     options = list(
@@ -160,8 +130,9 @@ server <- function(input, output, session) {
     escape = FALSE
   )
   
+  ## anchor table
   output$anchor_table <- renderDataTable(
-    Anchor_Table()[[1]],
+    anchor_table()[[1]],
     options = list(
       pageLength = 25,
       searching = T,
@@ -170,8 +141,9 @@ server <- function(input, output, session) {
     escape = FALSE
   )
   
+  ## target table
   output$target_table <- renderDataTable(
-    Target_Table(),
+    target_table(),
     options = list(
       pageLength = 25,
       searching = T,
@@ -180,8 +152,19 @@ server <- function(input, output, session) {
     escape = FALSE
   )
   
+  ## show execution time
   output$print_time <- renderUI(
     output_Table()[[2]]
   )
   
+  ## download output df
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste0(Sys.Date(), "_output", ".tsv")
+    },
+    content = function(file) {
+      write.csv(output_Table()[[1]], file, sep = "\t",
+                quote = FALSE, row.names = FALSE)
+    }
+  )
 }
