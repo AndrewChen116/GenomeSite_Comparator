a463f5cd5dd26fefe02dd79b009142ffd0374e60
AndrewChen116 <andrew199786821@gmail.com>
2022-03-16 12:40:33 +0800

    standardize the name of terms

--- ui.R
+++ ui.R
@@ -23,38 +23,39 @@ ui <- navbarPage(
   sidebarLayout(
     # side panel
     sidebarPanel(
+      ## file input
       fileInput(
         "target_file", h6(strong("Target file"), style = "color:#97CBFF"),
         accept = ".tsv", multiple = F ,width = "100%",
         placeholder = "Please input tsv data "
       ),
-      #htmlOutput("print_A") %>% h6(.,align="center",style = "color:#00BB00"),
       fileInput(
         "anchor_file", h6(strong("Anchor file"),style = "color:#97CBFF"),
         accept = ".tsv", multiple = F ,width = "100%",
         placeholder = "Please input tsv data "
       ),
-      #htmlOutput("print_T") %>% h6(.,align="center",style = "color:#00BB00"),
+      ## range setting
       fluidRow(
         column(
           numericInput(
-            "f_nt",h6(strong("Upstream range"),style = "color:#97CBFF"),
+            "up_nt",h6(strong("Upstream range"),style = "color:#97CBFF"),
             value = 20, min = 0, max = 1000000
           ),
           width = 6
         ),
         column(
           numericInput(
-            "b_nt",h6(strong("Downstream range"),style = "color:#97CBFF"),
+            "down_nt",h6(strong("Downstream range"),style = "color:#97CBFF"),
             value = 20, min = 0, max = 1000000
           ),
           width = 6
         )
       ),
+      ## Parallel setting
       fluidRow(
         column(
           selectInput(
-            "DoParallel",h6(strong("Parallel calculating"),style = "color:#97CBFF"),
+            "doParallel",h6(strong("Parallel calculating"),style = "color:#97CBFF"),
             choices = c("True", "False"),selected = "False"
           ),
           width = 6
@@ -68,35 +69,46 @@ ui <- navbarPage(
         )
       ),
       br(),
+      ## output setting
       fluidRow(
         column(
-          actionButton(inputId="DoSearch", label=" Search",
+          actionButton(inputId="doSearch", label=" Search",
                        icon=icon(name = "compass")),
           width = 3
         ),
         column(
-          actionButton(inputId="DoExample", label=" Example",
+          actionButton(inputId="doExample", label=" Example",
                        icon=icon(name = "table")),
           width = 3
         ),
         column(
-          width = 6
-          
+          downloadButton("downloadData", "Download"),
+          width = 3
+        ),
+        column(
+         
+          width = 3
+
         )
       ),
+      ## print execution time
       htmlOutput("print_time") %>% h6(.,align="center",style = "color:#B3D9D9"),
       br(),
       br(),
       br(),
       br(),
+      ## version info
       h6("20220315_KLC_v0.1.0",align="right",style = "color:#6C6C6C"),
+      ## width of sidebarPanel
       width = 3
       
     ),
     # main panel
     mainPanel(
+      ## tag setting
       tags$head(tags$style(HTML(css))),
       
+      ## output table
       tabsetPanel(
         tabPanel(
           h5("Comparison result",style = "color:#97CBFF"),
@@ -104,16 +116,17 @@ ui <- navbarPage(
           width = 1
         ),
         tabPanel(
-          h5("Target teble",style = "color:#97CBFF"),
+          h5("Target table",style = "color:#97CBFF"),
           dataTableOutput("target_table"),
           width = 1
         ),
         tabPanel(
-          h5("Anchor teble",style = "color:#97CBFF"),
+          h5("Anchor table",style = "color:#97CBFF"),
           dataTableOutput("anchor_table"),
           width = 1
         ),
       ),
+      ## width of mainPanel
       width = 9
     )
   )
