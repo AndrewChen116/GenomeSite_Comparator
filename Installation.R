35e6777152b0c7d221e166b6b6c033b276a14d93
AndrewChen116 <andrew199786821@gmail.com>
2022-03-16 12:42:06 +0800

    installation guideline for this tool

---
+++ Installation.R
@@ -0,0 +1,12 @@
+# Step 1: Please check your current R version, R version 4.0 or greater is required.
+  version
+
+# Step 2: Installation
+  install.packages("shiny")
+  install.packages("tidyverse")
+  install.packages("parallel")
+  install.packages("parallel")
+  install.packages("bslib")
+  install.packages("DT")
+  
+  
